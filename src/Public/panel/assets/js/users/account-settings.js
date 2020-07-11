eaa77924cb8404f255c25805905b495a698896cc
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-07-11 17:45:09 +0430

    update: update account-settings.js just for setting account-settings.blade

--- src/Public/panel/assets/js/users/account-settings.js
+++ src/Public/panel/assets/js/users/account-settings.js
@@ -26,30 +26,6 @@ jQuery(document).ready(function($) {
     });
 
     setTimeout(function(){ $('.list-group-item.list-group-item-action').last().removeClass('active'); }, 100);
-
-    $('#update-profile-setting').click(function (e) {
-        e.preventDefault();
-        var formdata = $(".general-info").serializeArray();
-        $.ajax({
-            url: "http://127.0.0.1:8000/panel/setting",
-            type: "POST",
-            data: formdata,
-            success: function(e) {
-                toast({
-                    type: 'success',
-                    title: e,
-                    padding: '2em',
-                })
-            },
-            error: function(e) {
-                toast({
-                    type: 'error',
-                    title: e.responseText,
-                    padding: '2em',
-                })
-            }
-        });
-    });
 });
 
 progressBarCount('.progress-range-counter');
