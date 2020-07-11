7991b1bd08bdc66b59d35d6c8205d066ba816a1d
HosseinAlikhani <poshtiban.esteam@gmail.com>
2020-07-11 17:42:07 +0430

    update: add new message for transitation

--- src/Lang/en/trans.php
+++ src/Lang/en/trans.php
@@ -4,6 +4,9 @@ return [
     'MyProfile' =>  'My Profile',
     'LogOut'    =>  'Log Out',
     'Dashboard' =>  'Dashboard',
+    'UserInterface' =>  'User Interface',
+    'User'  =>  'Users',
+    'Role'  =>  'Role - Permission',
     'GeneralInformation'    =>  'General Information',
     'UploadPicture' =>  'Upload Picture',
     'FirstName' =>  'First Name',
@@ -19,13 +22,19 @@ return [
     'Phone' =>  'Phone',
     'Email' =>  'E-Mail',
     'Website'   =>  'WebSite',
+    'View'  =>  'View',
+    'Create'    =>  'Create',
     'Update'    =>  'Update',
+    'Delete'    =>  'Delete',
     'Info'  =>  'Information',
     'Eduction'  =>  'Eduction',
     'workExperience'    =>  'Work Experience',
     'Skills'    =>  'Skills',
     'Bio'   =>  'Bio',
-
+    'Operation' =>  'Operation',
+    'State' =>  'State',
+    'Online'    =>  'Online',
+    'Offline'   =>  'Offline',
 
 
 
@@ -41,6 +50,8 @@ return [
 
     'message'   =>  [
         'updateok'  =>  'Your information has been successfully updated',
-        'updateno'  =>  'We have encountered an error in updating your information'
+        'updateno'  =>  'We have encountered an error in updating your information',
+        'deleteok'  =>  'Your information has been successfully deleted',
+        'deleteno'  =>  'We have encountered an error deleting your information',
     ]
 ];
